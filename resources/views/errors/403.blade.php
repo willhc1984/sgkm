5eb15ef0f21a9a6591d80547e0fda551722d99a6
will henrique <willhc1984@gmail.com>
2024-08-05 10:00:51 -0300

    Paginas de erro 403/404

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,1 +1,10 @@
-<h1>Sem permissão</h1>
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid px-4">
+        <div class="row mt-5">
+            <div class="d-flex justify-content-center mt-4 mb-4"> <img src="{{ asset('/img/lock3.png') }}" /></div>
+            <h3 class="text-center font-weight-light my-1 mt-4">Sem permissão!</h3>
+        </div>
+    </div>
+@endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,11 @@
-<h1>Pagina não existe</h1>
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid px-4">
+        <div class="row mt-5">
+            <div class="d-flex justify-content-center mt-4 mb-4"> <img src="{{ asset('/img/exclamation.png') }}" /></div>
+            <h3 class="text-center font-weight-light my-1 mt-4">Página não encontrada!</h3>
+        </div>
+    </div>
+@endsection
+
